bd92697c2c7fecf922a56a4069f40f579833c640
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2022-11-06 02:37:44 +0530

    test

--- router/hospitalRouter.js
+++ router/hospitalRouter.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 
 const router = Router();
 // List of docters in hospital
-// 
+// Add Docters
+// view details of the users and list of users under the doc
 module.exports = router;
