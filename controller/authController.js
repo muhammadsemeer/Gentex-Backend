c770bf3f2d9d0731441f20f74120d3f827725d69
JOBIN S <jobins9633@gmail.com>
2022-11-06 04:18:45 +0530

    completed login api

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,9 @@
+const User = require('../model/User');
+const jwt = require("jsonwebtoken")
+
+
 module.exports = {
-    login: (req, res) => {
+    login: async (req, res) => {
         const { email, password } = req.body;
         if (!(email && password)) {
             return res.status(400).json({
@@ -7,6 +11,28 @@ module.exports = {
             });
         }
 
-        return res.status(200).json({ message: "Login" });
+        const isUserExisting = await User.findOne({ email })
+
+        if (!isUserExisting) {
+            return res.status(400).json({
+                message: "User not found",
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, isUserExisting.password)
+
+        if(!isPasswordCorrect) {
+            return res.status(400).json({
+                message: "Invalid Crendentials",
+            });
+        }
+
+        const token = jwt.sign({
+            userId: isUserExisting._id,
+            role: isUserExisting.role,
+        }, process.env.JWT_SECRET || "secret")
+        
+
+        return res.status(200).json({ message: "Login Successfull", token });
     },
 };
