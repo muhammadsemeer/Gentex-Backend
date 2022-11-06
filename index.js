85d8558cdf89fee895296efc127d92607548cd9b
JOBIN S <jobins9633@gmail.com>
2022-11-06 10:36:19 +0530

    changed port added cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectDB = require("./utils/db");
 const server = http.createServer(app);
 
 // Setting PORT
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.set("port", PORT);
 
 server.listen(PORT);
--- validations/auth.js
+++ validations/auth.js
@@ -5,11 +5,7 @@ const login = [
     body("email").isEmail().withMessage("Email Id can't be blank"),
     body("password")
         .isStrongPassword({
-            minLength: 8,
-            minLowercase: 1,
-            minSymbols: 1,
-            minNumbers: 1,
-            minUppercase: 1,
+            minLength: 4,
         })
         .withMessage(
             "Password Must Have Min 8 characters and contain atleast 1 lowercase, 1 symbols, 1 numbers, 1 uppercase"
