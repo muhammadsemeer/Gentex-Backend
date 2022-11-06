1a7bd537056d093db9862f88910c1406e6faf238
JOBIN S <jobins9633@gmail.com>
2022-11-06 07:10:47 +0530

    handled mongo connect

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = () =>
-    mongoose.connect(
-        process.env.MONGO_URL,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        },
-        () => {
-            console.log("MongoDB Connected");
-        },
-        (e) => console.log(e)
-    ); // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=>{
+        console.log("connected successfully")
+    }).catch((e)=>{
+        console.log("DB Error", e)
+    })
 
 module.exports = connectDB;
